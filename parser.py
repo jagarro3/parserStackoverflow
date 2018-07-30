83cfc8f0850a5f89c29defb229f2632f4ff4f66c
Javier <javiergr93@gmail.com>
2018-07-30 15:14:33 +0200

    Refactor

--- parser.py
+++ parser.py
@@ -1,19 +1,11 @@
-import sys
 import os
+import sys
 import time
 import xml.etree.ElementTree as ET
-from connectMongoDB import connectionMongoDB
 from xml.dom import minidom
+from connectMongoDB import connectionMongoDB
 
-
-
-# def parsePostXml(file):
-#     tree = ET.parse(file)
-#     root = tree.getroot()
-#     for child in root:
-#         savePosts(child.attrib)
-
-def parsePostXml1(file):
+def parserXml(file):
     for event, element in ET.iterparse(file):
         if element.tag == 'row':
             savePosts(element.attrib)
@@ -25,11 +17,11 @@ def savePosts(dictionary):
 if __name__ == "__main__":
     os.system("cls")
     if len(sys.argv) < 3:
-        print("Introduce el fichero xml y el nombre de la base de datos")
-        print("-> Ejemplo: python .\parserXml.py [fichero.xml] [nombre]")
+        print("Introduce el fichero xml y el nombre de la colección MongoDB")
+        print("-> Ejemplo: python .\parser.py fichero.xml nombreColeccion")
     else:
         coleccion = connectionMongoDB(sys.argv[2])
         start_time = time.time()
-        parsePostXml1(sys.argv[1])
+        parserXml(sys.argv[1])
         elapsed_time = time.time() - start_time
         print("---->Tiempo ejecución:", elapsed_time)
